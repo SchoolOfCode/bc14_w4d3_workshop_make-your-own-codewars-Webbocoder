af83315716c9159145962104d3a060b23795584c
Christopher Webb <webbophone@gmail.com>
2023-04-09 17:50:24 +0100

    Scenario conjured.

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 /* 
 👉 Write your kata here!
+
+Oh no, an asteroid collided with the rainbow and now all its colours are jumbled up!
+Quick, you've got to put them back in the right order: "red, orange, yellow, green, blue, indigo, violet"!
+
+The output case needs to match the input case.
+Sometimes, colours in the input array will be superfluous and will need to be omitted.
+Sometimes, colours in the input array will be omitted; if this is the case, should should be no gap in the output array.
+
+For example:
+The Input: ["rEd", "yelLow", "greeN", "blue", "purPle", "iNdigo", "voileT"]
+Should return:
+["rEd", "yelLow", "greeN", "blue", "iNdigo", "voileT"]
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function ROYGBIV(input) {
+
+}
